9eccd9d46d9bb623a71a45d156f64ef7ca3a77b0
Avadhesh Mishra <avadhesh.cs@gmail.com>
2020-12-30 13:36:33 +0000

    initial git commit

---
+++ src/test/java/com/kubematics/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kubematics.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
